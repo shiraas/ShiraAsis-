7973f5e8da76318504e2772467df9edca70e9696
shiraas <shirar1201@gmail.com>
2017-03-27 15:41:29 +0300

    ex5

--- ShiraAsis/src/Algorithm/Multiplication.java
+++ ShiraAsis/src/Algorithm/Multiplication.java
@@ -8,12 +8,16 @@ public class Multiplication extends Encryption<Integer> {
 
     @Override
     public int encrypt(int oneByte, Integer key) {
+        if (key%2 == 0)
+            key = key+1;
         oneByte = (key * oneByte) & 0x000000FF;
         return oneByte;
     }
 
     @Override
     public int decrypt(int oneByte, Integer key) {
+        if (key%2 == 0)
+            key = key+1;
         int decKey = 0;
         for (int i = 0; i < 256; i++) {
             if(((i*key)& 0x000000FF)==1) {
